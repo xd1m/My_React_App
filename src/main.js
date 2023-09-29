87d21aa3c0152a28fb510a10dda4c1dca8f819bb
xd1m <hassanalzoubi47@gmail.com>
2023-09-29 11:30:55 +0300

    added main.css file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import data from './data.json'
 import CardComp from './card';
+import './main.css';
 function Main() {
     return (
         <>
-        <div style={{display : "flex" , flexWrap:"wrap" , justifyContent:'space-between', gap:"1px", marginTop:"3%"}}>
+        <div id='container'>
         {data.map(function(item){
             return(
                 <CardComp image={item.image_url} title={item.title} description={item.description}/>
